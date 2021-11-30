99519caeae2678aa450eb9a507e432225bbb0fd8
LaoLittle <y1941803875@outlook.com>
2021-11-30 17:45:59 +0800

    增加部分数据

--- src/main/kotlin/model/CaiyunData.kt
+++ src/main/kotlin/model/CaiyunData.kt
@@ -3,6 +3,7 @@ package org.laolittle.plugin.caiyun.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 
 @Serializable
@@ -17,8 +18,30 @@ data class UserData(
 )
 
 @Serializable
-data class UserInfoData(
+data class Data(
     @SerialName("data") val data: JsonElement
+)
+
+@Serializable
+data class FirstNode(
+    @SerialName("firstnode") val firstNode: JsonElement
+)
+
+@Serializable
+data class NodeId(
+    @SerialName("nid") val nid: String,
+    @SerialName("nodeid") val nodeId: String
+)
+
+@Serializable
+data class Novel(
+
+    @SerialName("content") val content: String
+)
+
+@Serializable
+data class Nodes(
+    @SerialName("nodes") val nodes: JsonArray
 )
 
 @Serializable
