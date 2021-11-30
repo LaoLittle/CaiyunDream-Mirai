6dcb5523ff281e19f973e79428ddadcaf2b3915f
LaoLittle <y1941803875@outlook.com>
2021-11-30 17:48:04 +0800

    重载配置

--- src/main/kotlin/CaiyunAI.kt
+++ src/main/kotlin/CaiyunAI.kt
@@ -4,12 +4,14 @@ import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.subscribeFriendMessages
+import net.mamoe.mirai.event.subscribeGroupMessages
 import net.mamoe.mirai.event.whileSelectMessages
 import net.mamoe.mirai.message.data.content
 import net.mamoe.mirai.utils.info
-import org.laolittle.plugin.caiyun.Config.userId
 import org.laolittle.plugin.caiyun.api.CaiyunApiService.loginFromCode
 import org.laolittle.plugin.caiyun.api.CaiyunApiService.sendVerification
+import org.laolittle.plugin.caiyun.api.CaiyunApiService.getNovelId
+import org.laolittle.plugin.caiyun.api.CaiyunApiService.startWrite
 
 object CaiyunAI : KotlinPlugin(
     JvmPluginDescription(
@@ -34,7 +36,6 @@ object CaiyunAI : KotlinPlugin(
                         }
                             val phoneNumber = this.message.content.toLong()
                             val codeId = sendVerification(phoneNumber)
-                        subject.sendMessage(codeId)
                             subject.sendMessage("请在两分钟内输入你所收到的验证码")
                             whileSelectMessages {
                                 default Here@{
@@ -42,8 +43,8 @@ object CaiyunAI : KotlinPlugin(
                                         subject.sendMessage("请输入正确的验证码")
                                         return@Here true
                                     }
-                                    loginFromCode(codeId, this.message.content.toInt(), phoneNumber)
-                                    subject.sendMessage(userId)
+                                    subject.sendMessage(loginFromCode(codeId, this.message.content.toInt(), phoneNumber))
+                                    Config.reload()
                                     false
                                 }
                                 timeout(120_000){
@@ -60,6 +61,38 @@ object CaiyunAI : KotlinPlugin(
                 }
             }
 
+        }
+        GlobalEventChannel.subscribeGroupMessages {
+            "te" {
+                subject.sendMessage(getNovelId("1233", "1244", true))
+            }
+            "续写" {
+                subject.sendMessage("请输入标题")
+                whileSelectMessages {
+                    default {
+                        val title = this.message.content
+                        subject.sendMessage("请输入你想要续写的正文")
+                        whileSelectMessages {
+                            default {
+                                val msg = this.message.content
+                                var nodeId = getNovelId(title, msg, true)
+                                val nid = getNovelId(title, msg, false)
+                                subject.sendMessage(msg + startWrite(title, msg, nodeId, nid))
+                                false
+                            }
+                            timeout(120_000){
+                                subject.sendMessage("超时")
+                                false
+                            }
+                        }
+                        false
+                    }
+                    timeout(10_000){
+                        subject.sendMessage("超时未输入")
+                        false
+                    }
+                }
+            }
         }
     }
 }
