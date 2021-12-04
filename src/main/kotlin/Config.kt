4a79e940ce7358162902642005096a0596b96039
LaoLittle <y1941803875@outlook.com>
2021-12-04 09:35:14 +0800

    添加模型ID

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -5,6 +5,8 @@ import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
 
 object Config : AutoSavePluginConfig("CaiyunConfig") {
-    @ValueDescription("UserId")
+    @ValueDescription("用户ID")
     var userId: String by value("")
+    @ValueDescription("使用的续写模型")
+    var modelId: String by value("60094a2a9661080dc490f75a")
 }
