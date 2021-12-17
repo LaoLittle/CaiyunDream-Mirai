831e09b459a1b9329a8f3b9926bdf3d1a1bea79d
LaoLittle <y1941803875@outlook.com>
2021-12-17 18:51:34 +0800

    验证码错误可重复输入

--- src/main/kotlin/messageService/LoginService.kt
+++ src/main/kotlin/messageService/LoginService.kt
@@ -4,7 +4,6 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.subscribeFriendMessages
 import net.mamoe.mirai.event.whileSelectMessages
-import net.mamoe.mirai.message.data.content
 import org.laolittle.plugin.caiyun.Service
 import org.laolittle.plugin.caiyun.api.CaiyunApiService
 import org.laolittle.plugin.caiyun.utils.onEnable
@@ -19,11 +18,11 @@ object LoginService : Service() {
                 subject.sendMessage("请在2分钟内输入你的手机号")
                 whileSelectMessages {
                     default {
-                        if (this.message.content.contains(Regex("""\D"""))) {
+                        if (it.contains(Regex("""\D"""))) {
                             subject.sendMessage("请输入正确的手机号")
                             return@default true
                         }
-                        phoneNumber = this.message.content.toLong()
+                        phoneNumber = it.toLong()
                         try {
                             codeId = CaiyunApiService.sendVerification(phoneNumber)
                         } catch (e: Exception) {
@@ -41,19 +40,26 @@ object LoginService : Service() {
                 subject.sendMessage("请在5分钟内输入你所收到的验证码")
                 whileSelectMessages {
                     default Here@{
-                        if (this.message.content.contains(Regex("""\D"""))) {
+                        if (it.contains(Regex("""\D"""))) {
                             subject.sendMessage("请输入正确的验证码")
                             return@Here true
                         }
-                        subject.sendMessage(
-                            CaiyunApiService.loginFromCode(
-                                codeId,
-                                this.message.content.toInt(),
-                                phoneNumber
+                        try {
+                            subject.sendMessage(
+                                CaiyunApiService.loginFromCode(
+                                    codeId,
+                                    it.toInt(),
+                                    phoneNumber
+                                )
                             )
-                        )
-                        if (!onEnable)
+                        } catch (e: Exception){
+                            subject.sendMessage("验证码错误！请重新输入")
+                            return@Here true
+                        }
+                        if (!onEnable) {
                             FollowUpListener.start()
+                            ModelSwitchListener.start()
+                        }
                         false
                     }
                     timeout(300_000) {
