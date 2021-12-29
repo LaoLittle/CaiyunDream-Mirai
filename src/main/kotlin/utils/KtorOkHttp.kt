5baa0cb891c78bb461048985d49708e7f2bb98f2
LaoLittle <y1941803875@outlook.com>
2021-12-29 19:18:39 +0800

    change the configuration of read timeout

--- src/main/kotlin/utils/KtorOkHttp.kt
+++ src/main/kotlin/utils/KtorOkHttp.kt
@@ -1,19 +1,22 @@
 package org.laolittle.plugin.caiyun.utils
 
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.*
+import io.ktor.client.engine.*
+import io.ktor.client.engine.okhttp.*
 import io.ktor.client.request.*
 import kotlinx.serialization.json.JsonElement
+import okhttp3.OkHttpClient
 import org.laolittle.plugin.caiyun.model.Json
+import java.util.concurrent.TimeUnit
 
-object KtorHttpUtil {
-    private val client = HttpClient(OkHttp)
+object KtorOkHttp {
+    private val client : HttpClient
     private const val ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.57"
 
     suspend fun get(url: String): JsonElement {
         return Json.parseToJsonElement(client.get{
             url(url)
-            header("Content-Type", "application/json;charset=utf-8")
+            header("Accept", "application/json")
             header("User-Agent", ua)
         })
 
@@ -27,5 +30,15 @@ object KtorHttpUtil {
             header("User-Agent", ua)
         }
         return Json.parseToJsonElement(responseData)
+    }
+
+    init {
+        OkHttpClient.Builder()
+        val okHttpEngine = OkHttp.config {
+            config {
+                readTimeout(30_000, TimeUnit.MILLISECONDS)
+            }
+        }
+        client = HttpClient(okHttpEngine)
     }
 }
